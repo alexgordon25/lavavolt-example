67f874194abb786e8f1befc851300a7e9cfd6a4b
Daniel Gordon <alexgordon25@gmail.com>
2014-09-18 09:42:36 -0600

    Pushing Lavavolt files into repo

---
+++ website skeleton/js/main.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    /* CAROUSEL
+    ========================================*/    
+    $("#myCarousel").carousel({
+        //interval: 8000,
+        //pause: true,
+        //interval: false
+    });
+}); //END DOCUMENT READY
